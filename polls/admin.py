10c499569d7cb5b97ee26cac37fa0e95d61e8986
samsunggora <samsunggora@gmail.com>
2020-05-15 11:39:10 +0200

    add hand text in admin.py

--- polls/admin.py
+++ polls/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
 from polls.models import Poll
 
-admin.site.register(Poll)
+from django.contrib import admin
+from polls.models import Poll
+
+
+class PollAdmin(admin.ModelAdmin):
+    fields = ['pub_date', 'question']
+
+
+admin.site.register(Poll, PollAdmin)
