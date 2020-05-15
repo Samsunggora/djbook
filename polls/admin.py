98504f708cb4e49a673d48c28053a760bdbb935d
samsunggora <samsunggora@gmail.com>
2020-05-15 11:50:38 +0200

    make pep8 rules

--- polls/admin.py
+++ polls/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from polls.models import Poll
-from polls.models import Choice
+from polls.models import Choice, Poll
 
-admin.site.register(Choice)
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
 
 
 class PollAdmin(admin.ModelAdmin):
@@ -10,6 +12,7 @@ class PollAdmin(admin.ModelAdmin):
         (None, {'fields': ['question']}),
         ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
     ]
+    inlines = [ChoiceInline]
 
 
 admin.site.register(Poll, PollAdmin)
