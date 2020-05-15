8ed38eac50dba0d499a2643be8c06b9a82a4f378
samsunggora <samsunggora@gmail.com>
2020-05-15 11:39:25 +0200

    commit down

--- polls/admin.py
+++ polls/admin.py
@@ -1,12 +1,4 @@
 from django.contrib import admin
 from polls.models import Poll
 
-from django.contrib import admin
-from polls.models import Poll
-
-
-class PollAdmin(admin.ModelAdmin):
-    fields = ['pub_date', 'question']
-
-
-admin.site.register(Poll, PollAdmin)
+admin.site.register(Poll)
