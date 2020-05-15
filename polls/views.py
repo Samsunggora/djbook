81b15f7eece0fed14193e355c20a36aa93d22c44
samsunggora <samsunggora@gmail.com>
2020-05-15 12:47:56 +0200

    rty sturt app

--- polls/views.py
+++ polls/views.py
@@ -1,19 +1,22 @@
 from django.http import HttpResponse
 from django.template import RequestContext, loader
 from polls.models import Poll
+from django.shortcuts import render
+from django.http import Http404
 
 
 def index(request):
-    latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context = RequestContext(request, {
-        'latest_poll_list': latest_poll_list,
-    })
-    return HttpResponse(template.render(context))
+    latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
+    context = {'latest_poll_list': latest_poll_list}
+    return render(request, 'polls/index.html', context)
 
 
 def detail(request, poll_id):
-    return HttpResponse("You're looking at poll %s." % poll_id)
+    try:
+        poll = Poll.objects.get(pk=poll_id)
+    except Poll.DoesNotExist:
+        raise Http404
+    return render(request, 'polls/detail.html', {'poll': poll})
 
 
 def results(request, poll_id):
@@ -22,3 +25,4 @@ def results(request, poll_id):
 
 def vote(request, poll_id):
     return HttpResponse("You're voting on poll %s." % poll_id)
+
