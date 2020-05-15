b56b84ef7544b8907550212001a0a806e358e1b1
samsunggora <samsunggora@gmail.com>
2020-05-15 11:58:08 +0200

    make add list filter

--- polls/admin.py
+++ polls/admin.py
@@ -8,6 +8,7 @@ class ChoiceInline(admin.TabularInline):
 
 
 class PollAdmin(admin.ModelAdmin):
+    list_filter = ['pub_date']
     list_display = ('question', 'pub_date', 'was_published_recently')
     fieldsets = [
         (None, {'fields': ['question']}),
