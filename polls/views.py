0ec4fcd537a4a17cc7671c969c4d09b9562e97c7
samsunggora <samsunggora@gmail.com>
2020-05-15 12:30:37 +0200

    Add pos in index

--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from polls.models import Poll
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the poll index.")
+    latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
+    output = ', '.join([p.question for p in latest_poll_list])
+    return HttpResponse(output)
 
 
 def detail(request, poll_id):
@@ -16,3 +19,4 @@ def results(request, poll_id):
 
 def vote(request, poll_id):
     return HttpResponse("You're voting on poll %s." % poll_id)
+
