e1d8d82f3e394ca756ed0f98b3d5ad69dee9902e
samsunggora <samsunggora@gmail.com>
2020-05-15 11:14:45 +0200

    add unicode and timezone

--- polls/models.py
+++ polls/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.utils import timezone
+import datetime
 
 
 class Poll(models.Model):
@@ -8,8 +10,14 @@ class Poll(models.Model):
     def __unicode__(self):
         return self.question
 
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
 
 class Choice(models.Model):
     poll = models.ForeignKey(Poll)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+    def __unicode__(self):
+        return self.choice_text
